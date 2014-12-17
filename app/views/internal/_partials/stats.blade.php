7e2180aa63ca3d3e3b3d9c7a0afc407ab62e4a1d
Karolis Ticka <ticka@inbox.lt>
2014-12-17 20:41:34 +0200

    Ok

--- app/views/internal/_partials/stats.blade.php
+++ app/views/internal/_partials/stats.blade.php
@@ -1,12 +1,12 @@
 @if ($stats)
 	@foreach($stats['playerStatSummaries'] as $stat)
 		<table width="100%">
-		<tr><th>Type</th><th>Wins</th></tr>
-		<tr><td>{{ $stat['playerStatSummaryType'] }}</td><td>{{ $stat['wins'] }}</td></tr>
+		<tr><th align="left">Type</th><th align="right">Wins</th></tr>
+		<tr><td align="left">{{ $stat['playerStatSummaryType'] }}</td><td align="right">{{ $stat['wins'] }}</td></tr>
 		@if (count($stat['aggregatedStats']) > 0)
-			<tr><th>Name</th><th>Count</th></tr>
+			<tr><th align="left">Name</th><th align="right">Count</th></tr>
 			@foreach($stat['aggregatedStats'] as $key => $details)
-				<tr><td><b>{{$key}}</b></td><td>{{$details}}</td></tr>
+				<tr><td align="left"><b>{{$key}}</b></td><td align="right">{{$details}}</td></tr>
 			@endforeach
 		@endif
 		</td></tr>
