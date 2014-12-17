e6efcc866a6bdccc6c5fd6eefdd5315fdf6bb976
Karolis Ticka <ticka@inbox.lt>
2014-12-17 20:44:50 +0200

    Ok

--- app/views/internal/_partials/user.blade.php
+++ app/views/internal/_partials/user.blade.php
@@ -1,4 +1,4 @@
 <table class="table" width="100%">
-	<tr><th>Image</th><th>Name</th><th>Level</th></tr>
-	<tr><td><img width="60px" src="http://avatar.leagueoflegends.com/eune/{{ $player['name'] }}.png"></td></td><td>{{ $player['name'] }}</td><td>{{ $player['summonerLevel'] }}</td></tr>
+	<tr><th class="text-left">Image</th><th class="text-left">Name</th><th class="text-right">Level</th></tr>
+	<tr><td class="text-left"><img width="60px" src="http://avatar.leagueoflegends.com/eune/{{ $player['name'] }}.png"></td><td class="text-left">{{ $player['name'] }}</td><td class="text-right">{{ $player['summonerLevel'] }}</td></tr>
 </table>
